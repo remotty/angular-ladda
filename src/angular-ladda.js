1296efb18967f23b8cb881a413e12c333d3349f5
Andrey Kalinichenko <kalinichenko@bifit.ua>
2014-10-31 16:53:37 +0200

    waiting untill the button is displayed

--- src/angular-ladda.js
+++ src/angular-ladda.js
@@ -11,29 +11,32 @@
     'ladda',
     [
       '$compile',
-      function ($compile) {
+      '$timeout',
+      function ($compile, $timeout) {
         return {
           restrict: 'A',
           link: function (scope, element, attrs) {
-            element.addClass('ladda-button');
-            if(angular.isUndefined(element.attr('data-style'))) {
-              element.attr('data-style', 'zoom-in');
-            }
-            var ladda = Ladda.create( element[0] );
-            $compile(angular.element(element.children()[0]).contents())(scope);
-
-            scope.$watch(attrs.ladda, function(loading) {
-              if(loading || angular.isNumber(loading)) {
-                if(!ladda.isLoading()) {
-                  ladda.start();
-                }
-                if(angular.isNumber(loading)) {
-                  ladda.setProgress(loading);
-                }
-              } else {
-                ladda.stop();
+            $timeout(function(){
+              element.addClass('ladda-button');
+              if(angular.isUndefined(element.attr('data-style'))) {
+                element.attr('data-style', 'zoom-in');
               }
-            });
+              var ladda = Ladda.create( element[0] );
+              $compile(angular.element(element.children()[0]).contents())(scope);
+
+              scope.$watch(attrs.ladda, function(loading) {
+                if(loading || angular.isNumber(loading)) {
+                  if(!ladda.isLoading()) {
+                    ladda.start();
+                  }
+                  if(angular.isNumber(loading)) {
+                    ladda.setProgress(loading);
+                  }
+                } else {
+                  ladda.stop();
+                }
+              });
+            }, 0);
           }
         };
       }
